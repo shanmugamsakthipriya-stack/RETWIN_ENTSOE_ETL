2017f8b55eb0db8eadc6c4db099cfe43315dec61
shanmugamsakthipriya-stack <shanmugam.sakthipriya@gmail.com>
2025-08-27 00:37:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+# ENTSOE
+ENTSOE_TOKEN = os.environ.get("ENTSOE_TOKEN")
+
+# Database
+AZURE_PG_HOST = os.environ.get("AZURE_PG_HOST")
+AZURE_PG_DB = os.environ.get("AZURE_PG_DB")
+AZURE_PG_USER = os.environ.get("AZURE_PG_USER")
+AZURE_PG_PASSWORD = os.environ.get("AZURE_PG_PASSWORD")
+
+# Email
+SMTP_SERVER = os.environ.get("SMTP_SERVER")
+SMTP_PORT = int(os.environ.get("SMTP_PORT", 587))
+SMTP_USER = os.environ.get("SMTP_USER")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
+ALERT_EMAIL = os.environ.get("ALERT_EMAIL")
